3618743ac319e77bddf213d3a1e50146518d280d
Sybihy78 <irishka_86.86@mail.ru>
2018-09-28 15:34:02 +0300

    First commit

---
+++ lab_work_4_rekursiya/lab_work_4_rekursiya.cpp
@@ -0,0 +1,141 @@
+# include <iostream>
+#include <clocale>
+
+
+int print(int start, int n);
+
+void exponent_two(int n);
+
+int fibo(int n1, int n2, int n, int x);
+
+int sum_digits(int sum, int n);
+
+int get_max(int n, int max);
+
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+	
+	std::cout << "Функция, которая выводит числа от 1 до n" << std::endl;
+	int number_1 = 1; 
+
+	std::cout << "Enter n" << std::endl;
+	int number_2 = 0;
+	std::cin >> number_2;
+	
+	std::cout << print(number_1, number_2) << std::endl;
+	
+	std::cout << "Функция, которая выводит слово YES, если число n является точной степенью двойки, ";
+	std::cout << "или слово NO в противном случае" << std::endl;
+	std::cout << "Enter digit" << std::endl; 
+	int digit = 0;
+	std::cin >> digit;
+	
+	exponent_two(digit);
+	
+	std::cout << "Функция, которая выводит заданное число Фибоначчи" << std::endl;
+	std::cout << "What number of Fibonacchi do you want to sum?" << std::endl; 
+	int number = 0;
+	std::cin >> number;
+	
+	int number1 = 0;
+	int number2 = 1;
+	int count = 3;
+
+	std::cout << fibo(number1, number2, number, count) << std::endl;
+	
+	std::cout << "Функция, которая выводит сумму цифр числа" << std::endl;
+	std::cout << "What number do you want to sum digits of?" << std::endl; 
+	int your_number = 0;
+	std::cin >> your_number;
+	
+	int sum1 = 0;
+
+	std::cout << sum_digits(sum1, your_number) << std::endl;
+	
+	std::cout << "Функция, которая выводит максимальное значение из последовательности чисел" << std::endl;
+	std::cout << "Enter numbers:" << std::endl; 
+	int numbers = 0;
+	
+	get_max(numbers, 0);
+
+	system("pause");
+	return 0;
+}
+
+
+int print(int start, int n) {
+
+	if (start == n) {
+		return n;
+	}
+
+	std::cout << start << std::endl;
+
+	return print(++start, n);
+}
+
+
+void exponent_two(int n) {
+
+	if (n == 1) {
+		std::cout << "YES" << std::endl;
+	}
+	else if (n % 2 != 0) {
+		std::cout << "NO" << std::endl;
+	}
+	else {
+		return exponent_two(n / 2);
+	}
+}
+
+
+int fibo(int n1, int n2, int n, int x) {
+
+	int sum = n1 + n2;
+
+	if (n == 0 || n == 1) {
+		return 0;
+	}
+	else if (n == 2) {
+		return 1;
+	}
+	else if (n == x) {
+		return sum;
+	}
+	else {
+		return fibo(n2, sum, n, ++x);
+	}
+}
+
+
+int sum_digits(int sum, int n) {
+
+	sum += n % 10;
+
+	if (n < 1) {
+		return sum;
+	}
+
+	return sum_digits(sum, n / 10);
+}
+
+
+int get_max(int n, int max) {
+
+	std::cin >> n;
+
+	if (n > max) {
+		max = n;
+	}
+
+	std::cout << "Max of your numbers is" << "\t" << max << std::endl;
+	
+	if (n == 0) {
+		return max;
+	}
+		
+	return get_max(n, max);
+}
+
